f330b5048f763830b8d24eec041da051cdb2ee32
Michal Rybecky <michalrybec@protonmail.com>
2021-04-09 10:24:43 +0200

    Samostatny python file na nastavenia ako WIN, vyska, sirka, icon_size atd

---
+++ settings.py
@@ -0,0 +1,12 @@
+import pygame
+
+HEIGHT = 900
+WIDTH = 500
+HEIGHT_H = HEIGHT // 2
+WIDTH_H = WIDTH // 2
+icon_size = 110
+icon_size_h = icon_size // 2
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
