b237a3c45559d829361d65d7422089703842b7ff
David K <david.krchnavy@gmail.com>
2021-04-09 09:51:29 +0200

    dokonceny vypocet urokov

--- utils/financna_matematika.py
+++ utils/financna_matematika.py
@@ -1,11 +1,22 @@
-def uroky(doba, vynos, interval='y', kapital=0, prispevok=0, dan=False, vyska_dane=0):
-    """
-    WORK IN PROGRESS
-    """
+def uroky(kapital, doba, urok, interval_uroku='y', prispevok=0, interval_prispevku='y', dan=False, vyska_dane=0):
     results = []
-    vynos, dan = vynos / 100, dan / 100
+    urok, dan = urok / 100, dan / 100
     if not dan:
         vyska_dane = 0
-    for i in range(doba):
-        results.append(kapital)
-        kapital += (kapital * vynos) - (kapital * vynos * dan)
+    if interval_uroku == interval_prispevku:
+        for i in range(doba+1):
+            results.append(round(kapital, 2))
+            kapital += (kapital * urok) - (kapital * urok * dan)
+            kapital += prispevok
+    elif interval_uroku == 'y' and interval_prispevku == 'm':
+        for i in range(doba+1):
+            results.append(round(kapital, 2))
+            kapital += prispevok * 12
+            kapital += (kapital * urok) - (kapital * urok * dan)
+    elif interval_uroku == 'm' and interval_prispevku == 'y':
+        for i in range(doba+1):
+            results.append(round(kapital, 2))
+            kapital += (kapital * urok) - (kapital * urok * dan)
+            if doba % 12 == 0 and doba != 0:
+                kapital += prispevok
+    return results
