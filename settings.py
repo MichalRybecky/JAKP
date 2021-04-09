a19b86c561344939a93177f4cc3330f9a992d991
Michal Rybecky <michalrybec@protonmail.com>
2021-04-09 12:36:36 +0200

    Nove srandy v settings

--- settings.py
+++ settings.py
@@ -2,11 +2,14 @@ import pygame
 
 HEIGHT = 900
 WIDTH = 500
+
 HEIGHT_H = HEIGHT // 2
 WIDTH_H = WIDTH // 2
-icon_size = 110
-icon_size_h = icon_size // 2
 
+ICON_SIZE = 110
+ICON_SIZE_H = ICON_SIZE // 2
+
+UI_COLOR = (204, 204, 204)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
