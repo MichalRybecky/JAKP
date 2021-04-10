def7b2750268b9604ba39977753af0ff45cb1c92
Michal Rybecky <michalrybec@protonmail.com>
2021-04-10 20:23:50 +0200

    Pridanie X a Menu ikon a buttonov, X uz funguje

--- main.py
+++ main.py
@@ -36,6 +36,9 @@ def main():
         B_MENY = pygame.Rect((WIDTH_H - ICON_SIZE_H) // 2, HEIGHT_H * 3 // 2 + 40, ICON_SIZE, ICON_SIZE)
         B_STOCKS = pygame.Rect((WIDTH_H - ICON_SIZE_H) // 2 * 3, HEIGHT_H * 3 // 2 + 40, ICON_SIZE, ICON_SIZE)
 
+        B_X = pygame.Rect(10, 10, 60, 60)
+        B_MENU = pygame.Rect(WIDTH - 60 - 10, 10, 60, 60)
+
         # ICONS
         WIN.blit(KEBAB, ((WIDTH_H - ICON_SIZE_H) // 2, HEIGHT_H // 3))
         WIN.blit(A_SORT, ((WIDTH_H - ICON_SIZE_H) // 2 * 3, HEIGHT_H // 3))
@@ -45,6 +48,9 @@ def main():
         WIN.blit(CALCUL, ((WIDTH_H - ICON_SIZE_H) // 2 * 3, HEIGHT_H + ICON_SIZE - 30))
         WIN.blit(MENY, ((WIDTH_H - ICON_SIZE_H) // 2, HEIGHT_H * 3 // 2 + 40))
         WIN.blit(STOCKS, ((WIDTH_H - ICON_SIZE_H) // 2 * 3, HEIGHT_H * 3 // 2 + 40))
+
+        WIN.blit(X, (10, 10))
+        WIN.blit(MENU, (WIDTH - 60 - 10, 10))
 
         # LABELS
         # label_kebab = main_font.render("Kebab", 1, (10, 10, 10))
@@ -77,6 +83,10 @@ def main():
                 meny_app()
             elif B_STOCKS.collidepoint(pos_x, pos_y):
                 print("STOCKS")
+            elif B_MENU.collidepoint(pos_x, pos_y):
+                print("MENU")    
+            elif B_X.collidepoint(pos_x, pos_y):
+                run = False    
 
         pygame.display.update()
         for event in pygame.event.get():
