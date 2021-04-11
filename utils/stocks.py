4f36eb282feb4ec2ded02d1b5480099f0d86a07c
David K <david.krchnavy@gmail.com>
2021-04-11 19:53:40 +0200

    Opraveny KeyError pri prazdnom stringu

--- utils/stocks.py
+++ utils/stocks.py
@@ -3,6 +3,8 @@ import json
 
 def get_price(symbol):
     "Returns JSON formatted price of a stock. Takes the stock symbol as a parameter."
+    if not symbol:
+        return "Unknown symbol"
     conn = http.client.HTTPSConnection("api.iextrading.com")
     conn.request("GET", "/1.0/tops/last?symbols="+symbol)
     res = conn.getresponse()
@@ -10,4 +12,4 @@ def get_price(symbol):
     try:
         return json.loads(data)[0]["price"]
     except IndexError:
-        return "Unknown symbol."
+        return "Unknown symbol"
