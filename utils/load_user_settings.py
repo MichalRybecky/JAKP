57cbdfdbed1084a24d0c508f19f39fd85b76a6e9
David K <david.krchnavy@gmail.com>
2021-04-17 17:23:41 +0200

    Opraveny error ak neexistuje subor s nastaveniami

--- utils/load_user_settings.py
+++ utils/load_user_settings.py
@@ -4,8 +4,13 @@ Module for loading user settings and returning them in a dictionary
 
 
 def return_user_settings() -> dict:
-    with open("user_settings.txt", "r") as file:
-        data = file.readline().strip().split(',')
-    result = {"theme": data[0], "background": data[1]}
-    return result
-
+    try:
+        with open("user_settings.txt", "r") as file:
+            data = file.readline().strip().split(',')
+        result = {"theme": data[0], "background": data[1]}
+        return result
+    except FileNotFoundError:
+        f = open("user_settings.txt", "w")
+        f.write("light,classic")
+        f.close()
+        return {"theme": 'light', "background": 'classic'}
