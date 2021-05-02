2a7d810db33f7e69a4d229d0930d22500edd43c3
David Krchnavy <david.krchnavy@gmail.com>
2021-05-02 13:00:14 +0200

    oprava chyb

--- utils/kvadraticka.py
+++ utils/kvadraticka.py
@@ -1,4 +1,5 @@
 def kvadraticka_rovnica(a, b, c):
     if type(a) != int or type(b) != int or type(c) != int:
         raise TypeError("Arguments must be of type int")
-    return [(-b + (b ** 2 - 4 * a * c) ** 0.5) / 2 * a, (-b - (b ** 2 - 4 * a * c) ** 0.5) / 2 * a]
+    diskriminant = b ** 2 - (4 * a * c)
+    return [diskriminant, ((-b) + diskriminant ** 0.5) / (2 * a), ((-b) - diskriminant ** 0.5) / (2 * a)]
