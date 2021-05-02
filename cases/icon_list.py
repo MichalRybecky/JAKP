c86453ee3311bebeae676990f6da0a9b24b4401a
Michal Rybecky <michalrybec@protonmail.com>
2021-05-02 14:29:05 +0200

    List vsetkych itemov v prvej kolekcii

---
+++ cases/icon_list.py
@@ -0,0 +1,36 @@
+"""
+Modul na drzanie informacii o itemoch
+meno itemu, meno ikonky, rarita
+"""
+from cases.load_items import *
+
+icon_list = [
+    {"name": "benjos_masterpiece", "rarity": 2, "icon": BENJOS_MASTERPIECE},
+    {"name": "berta", "rarity": 1, "icon": BERTA},
+    {"name": "birdhouse", "rarity": 2, "icon": BIRDHOUSE},
+    {"name": "black_marker", "rarity": 4, "icon": BLACK_MARKER},
+    {"name": "blue_marker", "rarity": 4, "icon": BLUE_MARKER},
+    {"name": "bonsai", "rarity": 2, "icon": BONSAI},
+    {"name": "chewed_pen", "rarity": 4, "icon": CHEWED_PEN},
+    {"name": "class_angel", "rarity": 4, "icon": CLASS_ANGEL},
+    {"name": "classmade_dart", "rarity": 3, "icon": CLASSMADE_DART},
+    {"name": "crucifix", "rarity": 2, "icon": CRUCIFIX},
+    {"name": "cursed_object", "rarity": 3, "icon": CURSED_OBJECT},
+    {"name": "eraser", "rarity": 4, "icon": ERASER},
+    {"name": "face_mask", "rarity": 4, "icon": FACE_MASK},
+    {"name": "founders_picture", "rarity": 1, "icon": FOUNDERS_PICTURE},
+    {"name": "gerta", "rarity": 1, "icon": GERTA},
+    {"name": "green_marker", "rarity": 4, "icon": GREEN_MARKER},
+    {"name": "grzes_angel", "rarity": 3, "icon": GRZES_ANGEL},
+    {"name": "helpful_ladybug", "rarity": 3, "icon": HELPFUL_LADYBUG},
+    {"name": "holy_bible", "rarity": 1, "icon": HOLY_BIBLE},
+    {"name": "laser_remote", "rarity": 2, "icon": LASER_REMOTE},
+    {"name": "monstromon", "rarity": 3, "icon": MONSTROMON},
+    {"name": "petroleum_mirror", "rarity": 2, "icon": PETROLEUM_MIRROR},
+    {"name": "red_marker", "rarity": 4, "icon": RED_MARKER},
+    {"name": "saints_hat", "rarity": 3, "icon": SAINTS_HAT},
+    {"name": "shamlik", "rarity": 3, "icon": SHAMLIK},
+    {"name": "soap", "rarity": 4, "icon": SOAP},
+    {"name": "toilet_paper", "rarity": 4, "icon": TOILET_PAPER},
+    {"name": "wanted_individual", "rarity": 3, "icon": WANTED_INDIVIDUAL},
+]
