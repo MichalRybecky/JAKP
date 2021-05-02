18f61db3cc1a03aeed9ca389aec0b28a9f4a662a
Michal Rybecky <michalrybec@protonmail.com>
2021-05-02 21:20:06 +0200

    Pridanie buttonov na spustanie case openingu a cooldownu na prevenciu rychloklikania

--- apps/case_opening_menu.py
+++ apps/case_opening_menu.py
@@ -14,6 +14,8 @@ from settings import UI_COLOR, WIN, WIDTH, HEIGHT, WIDTH_H, HEIGHT_H, FPS
 from utils.user_settings_handling import return_user_settings
 from apps.settings_menu import settings_menu
 
+from apps.case_opening import case_opening_app
+
 
 def cases_app_opening():
     """
@@ -23,10 +25,15 @@ def cases_app_opening():
     click = False
     clock = pygame.time.Clock()
 
+    cooldown = 0
+
     # BUTTON INITIALIZATION
     B_INV_BACK = pygame.Rect(190, 205, 55, 55)
     B_INV_FORWARD = pygame.Rect(255, 205, 55, 55)
-    B_OPENING = pygame.Rect(110, 105, 280, 75)
+    B_TRIP_LABEL = pygame.Rect(110, 185, 280, 75)
+    B_TRIP_CASE = pygame.Rect(155, 270, 190, 150)
+    B_CLASS_LABEL = pygame.Rect(110, 445, 280, 75)
+    B_CLASS_CASE = pygame.Rect(155, 530, 190, 150)
     B_BACK = pygame.Rect(20, 20, 60, 60)
     B_MENU = pygame.Rect(WIDTH - 65 - 20, 20, 60, 60)
     
@@ -47,13 +54,16 @@ def cases_app_opening():
         #)
 
         # Zistovanie, ci nebolo kliknute na textove pole
-        if click:
+        if click and cooldown == 0:
             if B_BACK.collidepoint(pos_x, pos_y):
                 run = False
             elif B_MENU.collidepoint(pos_x, pos_y):
                 settings_menu()
-            #elif B_INV_FORWARD.collidepoint(pos_x, pos_y):
-            #    inv_forward()
+            elif B_TRIP_CASE.collidepoint(pos_x, pos_y) or B_TRIP_LABEL.collidepoint(pos_x, pos_y):
+                case_opening_app(case_type="trip")
+            elif B_CLASS_CASE.collidepoint(pos_x, pos_y) or B_CLASS_LABEL.collidepoint(pos_x, pos_y):
+                case_opening_app(case_type="class")
+            cooldown = FPS // 3
 
         # Event handling
         events = pygame.event.get()
@@ -66,6 +76,8 @@ def cases_app_opening():
             if event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     click = False
+        if cooldown != 0:
+            cooldown -= 1
 
         pygame.display.update()
         clock.tick(FPS)
